348f097dd9173b751d68f7a752d49460b1b291d7
Wojciech Zagórski <wojaqu@gmail.com>
2019-09-22 20:37:05 +0200

    add microservices for easy scaling in production

---
+++ cmd/services/count_chirps/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"chirperweb/internal/chirper"
+	"chirperweb/internal/routing"
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi"
+	"github.com/joho/godotenv"
+)
+
+const role = "admin"
+
+func init() {
+	godotenv.Load()
+}
+
+func main() {
+	provider, err := chirper.NewDefaultPostgresProvider()
+	provider.EnsureSchema()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	subRouter := chi.NewRouter()
+	subRouter.Use(routing.CheckPermissions(role))
+	subRouter.Get("/chirps/count", chirper.CountChirpsHandler(provider))
+
+	baseRouter := routing.NewBaseRouter()
+	baseRouter.Mount("/v1/api", subRouter)
+
+	walkFunc := func(method string, route string, handler http.Handler, middlewares ...func(http.Handler) http.Handler) error {
+		log.Printf("%s %s\n", method, route)
+		return nil
+	}
+	if err := chi.Walk(baseRouter, walkFunc); err != nil {
+		log.Panicf("Logging err: %s\n", err.Error())
+	}
+
+	log.Fatal(http.ListenAndServe(":8080", baseRouter))
+}
---
+++ cmd/services/post_chirp/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"chirperweb/internal/chirper"
+	"chirperweb/internal/routing"
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi"
+	"github.com/joho/godotenv"
+)
+
+func init() {
+	godotenv.Load()
+}
+
+func main() {
+	provider, err := chirper.NewDefaultPostgresProvider()
+	provider.EnsureSchema()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	subRouter := chi.NewRouter()
+	subRouter.Get("/admin/chirps/count", chirper.CreateChirpHandler(provider))
+
+	baseRouter := routing.NewBaseRouter()
+	baseRouter.Mount("/v1/api", subRouter)
+
+	walkFunc := func(method string, route string, handler http.Handler, middlewares ...func(http.Handler) http.Handler) error {
+		log.Printf("%s %s\n", method, route)
+		return nil
+	}
+	if err := chi.Walk(baseRouter, walkFunc); err != nil {
+		log.Panicf("Logging err: %s\n", err.Error())
+	}
+
+	log.Fatal(http.ListenAndServe(":8080", baseRouter))
+}
